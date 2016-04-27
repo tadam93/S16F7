6638dae4f6cb44de4aa36955858fd169bb01cf99
tadam93 <gophres@gmail.com>
2016-04-27 12:29:54 -0500

    corrected types in dml5

--- DML5.sql
+++ DML5.sql
@@ -6,7 +6,7 @@ truncate table P_Employee_Projects;
 truncate table P_Parent_Child;
 
 -- Disable the PK triggers on the sample data tables using the following syntax: 
-alter trigger P_Department_PK disable; -- for example - alter trigger DEPT_VIEW_TRIGGER disable; alter trigger P_Employee_Projects_PK disable; 
+alter trigger P_Department_PK disable; -- for example - alter trigger DEPT_VIEW_TRIGGER disable; alter trigger P_Employee_Projects_PK disable;
 alter trigger P_Parent_child_PK disable;
 alter trigger P_Person_PK disable;
 alter trigger P_Projects_PK disable;
@@ -25,16 +25,16 @@ alter table P_Projects disable constraint "P_Projects_P_Projects_FK”
 
 -- Put all of the sample data INSERT statements here. Be sure to use PK and FK values that -- are < 100 so that they won't be repeated by the PK triggers when they are re-enabled.
 
-INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type) 
-  VALUES (1, “Adam”, “Poncher”, “2700 Nueces TX”, 1, 100000, “Manager”);
-INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type) 
-  VALUES (2, “Trevor”, “Adams”, “2807 Rio TX”, 1, 140000, “President”);
-INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type) 
-  VALUES (3, “Victor”, “Maeteas”, “801 W. 24th TX”, 1, 60000, “Employee”);
-INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type) 
-  VALUES (4, “Conner”, “Col”, “801 W. 24th TX ”, 1, 80000, “Inter-Manager”);
-INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type) 
-  VALUES (5, “West”, “Hubble”, “810 N 6th TX”, 1, 30000, “Previous- Employee”);
+INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type)
+  VALUES (1, “Adam”, “Poncher”, “2700 Nueces TX”, 1, 100000, “manager”);
+INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type)
+  VALUES (2, “Trevor”, “Adams”, “2807 Rio TX”, 1, 140000, “president”);
+INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type)
+  VALUES (3, “Victor”, “Maeteas”, “801 W. 24th TX”, 1, 60000, “employee”);
+INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type)
+  VALUES (4, “Conner”, “Col”, “801 W. 24th TX ”, 1, 80000, “interim-manager”);
+INSERT into P_Person (person_id, first_name, last_name, home_address, us_citizen, salary, type)
+  VALUES (5, “West”, “Hubble”, “810 N 6th TX”, 1, 30000, “previous-employee”);
 
 INSERT into P_Projects (project_no, project_title) VALUES (1, “ALPHA”);
 INSERT into P_Projects (project_no, project_title) VALUES (2, “BETA”);
@@ -48,7 +48,7 @@ INSERT into P_Parent_child (p_person_person_id, P_person_person_id1) VALUES (3, 
 
 INSERT into P_Employee_projects (p_person_person_id, P_Projects_project_no) VALUES (1, 1);
 
--- Enable the PK triggers on the sample data tables using the following statement: alter trigger P_Department_PK enable; -- for example - alter trigger DEPT_VIEW_TRIGGER disable; alter trigger P_Employee_Projects_PK enable; 
+-- Enable the PK triggers on the sample data tables using the following statement: alter trigger P_Department_PK enable; -- for example - alter trigger DEPT_VIEW_TRIGGER disable; alter trigger P_Employee_Projects_PK enable;
 alter trigger P_Parent_child_PK enable;
 alter trigger P_Person_PK enable;
 alter trigger P_Projects_PK enable;
